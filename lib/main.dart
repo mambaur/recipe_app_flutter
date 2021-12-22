016455af8431cf4e12496b3412db54e113b5d687
mambaur <alwiroziq1998@gmail.com>
2021-12-23 00:29:37 +0700

    update view dashboard, detail recipe

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:recipe_app/home/dashboard.dart';
 
 void main() {
@@ -11,21 +12,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
     return MaterialApp(
       title: 'Resep Masakan',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+              iconTheme: IconThemeData(color: Colors.black),
+              actionsIconTheme: IconThemeData(color: Colors.black),
+              elevation: 1)),
       home: const Dashboard(),
     );
   }
---
+++ lib/utils/custom_snackbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+enum SnackbarType { success, warning, error }
+
+class CustomSnackbar {
+  static showSnackbar(BuildContext context, String message, SnackbarType type,
+      {SnackBarAction? snackBarAction}) {
+    final snackBar = SnackBar(
+      action: snackBarAction,
+      backgroundColor: type == SnackbarType.success
+          ? Colors.green.shade600
+          : type == SnackbarType.warning
+              ? Colors.orange.shade700
+              : Colors.red.shade700,
+      content: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+              type == SnackbarType.success
+                  ? Icons.check_circle
+                  : type == SnackbarType.warning
+                      ? Icons.info
+                      : Icons.error,
+              color: Colors.white),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+              child:
+                  Text(message, style: const TextStyle(color: Colors.white))),
+        ],
+      ),
+      // action: SnackBarAction(
+      //   label: 'Oke',
+      //   onPressed: () => ScaffoldMessenger.of(context).hideCurrentSnackBar(),
+      // ),
+    );
+
+    // Find the ScaffoldMessenger in the widget tree
+    // and use it to show a SnackBar.
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
